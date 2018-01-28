b474317362d77a091896ff3b42a39703ffa88a5b
milindl <imilindluthra@gmail.com>
2018-01-28 17:04:27 +0530

    Fix generics and comments

--- tests/6.go
+++ tests/6.go
@@ -2,17 +2,19 @@ package main;
 
 import "fmt";
 
-func<A, B> foldr(func f(a A, b B) B, id B, []A lst) B {
+// Comments...
+
+func < A:Doable $ Eatable   $    Shakeable,B: ChaitiLalal$ Chaiti , C> foldr(func f(a A, b B) B, id B, []A lst) B {
     if len(lst) == 0 {
         return id;
     }
     return f(head(A), foldr(f, id, tail(A)));
-}
+}  // end of function
 
 
 gen TillTen() int {
     for i := 0; i != 10; i++ {
         yield i
     }
-    return 0
+    return 0  // say hello to this world!
 }
