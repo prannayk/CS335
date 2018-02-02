e4c8bc5f99b58897d1097d6c2a9f9ef33c21c1d9
Abhibhav Garg <abhibhav.14@gmail.com>
2018-02-02 19:12:28 +0530

    Pushing basic structure

---
+++ src/codegen/ir.h
@@ -0,0 +1,30 @@
+#include "opcode.h"
+#include <string.h>
+
+class IR {
+private:
+
+public:
+	
+};
+
+class ComplexBlock {
+private:
+
+public:
+
+};
+
+class SimpleBlock {
+private:
+
+public:
+
+};
+
+class Instruction {
+private:
+
+public:
+
+};
---
+++ src/codegen/opcode.h
@@ -0,0 +1,6 @@
+enum opcode{
+	ADD=0,
+	SUB=1,
+	MUL=2,
+	DIV=3
+};
