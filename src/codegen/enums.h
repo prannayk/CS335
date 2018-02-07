5766f77d9f207bf0fc42230331694a68458bce14
Prannay Khosla <prannay.khosla@gmail.com>
2018-02-07 19:19:51 +0530

    Adding basic code for lexer, parsing and inserting statements

--- src/codegen/enums.h
+++ src/codegen/enums.h
@@ -6,12 +6,19 @@ enum OpCode
     SUB = 1,
     MUL = 2,
     DIV = 3,
+    SL = 4,
+    SR = 5,
+    GT = 6,
+    LT = 7,
+    GEQ = 8,
+    LEQ = 9,
+
 };
 
 enum AddressingMode
 {
     REGISTER = 0,
-    CONSTANT = 1,
+    CONSTANT_VAL = 1,
     ADDRESS = 2,
 };
 
